e07c48afb1275b31fc3a5176035eee9b99436e40
rebshess <rebeccashesser@gmail.com>
2023-02-22 13:42:29 -0500

    changed order of experience modules

--- app/routes/Experience.tsx
+++ app/routes/Experience.tsx
@@ -26,38 +26,33 @@ export default function Experience() {
         <NavLink
           onClick={(e) => {
             e.preventDefault();
-            if (openGreenhouse === true) setOpenGreenhouse(false);
-            if (openGreenhouse === false) setOpenGreenhouse(true);
+            if (openVisiQL) setOpenVisiQL(false);
+            if (!openVisiQL) setOpenVisiQL(true);
           }}
         >
           <div className='flex flex-col max-w-sm mx-4 my-6 shadow-lg hover:scale-105'>
             <div className='px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-zinc-800'>
               <p className='relative px-6 py-1 text-lg italic text-center dark:text-gray-100'>
-                Greenhouse is the hiring operating system for people-first
-                companies. Our software and structured hiring approach will help
-                you get better at hiring, so you have the technology you need to
-                take on what’s next.
+                VisiQL is an open-source project that I am working on with a
+                small team of engineers. The application is an all-in-one
+                GraphQL schema and resolver generator and SQL database
+                visualizer.
               </p>
             </div>
+
             <div className='flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-indigo-500 dark:text-gray-900'>
               <img
-                src={Greenhouse}
-                alt='Greenhouse Logo'
+                src={VisiQL}
+                alt='VisiQL Logo'
                 className='w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700'
               />
-              <p className='text-xl font-semibold leading-tight'>
-                Greenhouse Software
-              </p>
+              <p className='text-xl font-semibold leading-tight'>VisiQL</p>
               <p className='text-sm uppercase'>New York, NY</p>
-              <p>2015 - 2020</p>
+              <p>2022 - Present</p>
             </div>
           </div>
         </NavLink>
-        {/* @ts-ignore */}
-        <GreenhouseExp
-          openGreenhouse={openGreenhouse}
-          setOpenGreenhouse={setOpenGreenhouse}
-        />
+        <VisiQLExp openVisiQL={openVisiQL} setOpenVisiQL={setOpenVisiQL} />
         {/* @ts-ignore */}
         <NavLink
           onClick={(e) => {
@@ -93,37 +88,43 @@ export default function Experience() {
           openAttentive={openAttentive}
           setOpenAttentive={setOpenAttentive}
         />
+
         {/* @ts-ignore */}
         <NavLink
           onClick={(e) => {
             e.preventDefault();
-            if (openVisiQL) setOpenVisiQL(false);
-            if (!openVisiQL) setOpenVisiQL(true);
+            if (openGreenhouse === true) setOpenGreenhouse(false);
+            if (openGreenhouse === false) setOpenGreenhouse(true);
           }}
         >
           <div className='flex flex-col max-w-sm mx-4 my-6 shadow-lg hover:scale-105'>
             <div className='px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-zinc-800'>
               <p className='relative px-6 py-1 text-lg italic text-center dark:text-gray-100'>
-                VisiQL is an open-source project that I am working on with a
-                small team of engineers. The application is an all-in-one
-                GraphQL schema and resolver generator and SQL database
-                visualizer.
+                Greenhouse is the hiring operating system for people-first
+                companies. Our software and structured hiring approach will help
+                you get better at hiring, so you have the technology you need to
+                take on what’s next.
               </p>
             </div>
-
             <div className='flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-indigo-500 dark:text-gray-900'>
               <img
-                src={VisiQL}
-                alt='VisiQL Logo'
+                src={Greenhouse}
+                alt='Greenhouse Logo'
                 className='w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700'
               />
-              <p className='text-xl font-semibold leading-tight'>VisiQL</p>
+              <p className='text-xl font-semibold leading-tight'>
+                Greenhouse Software
+              </p>
               <p className='text-sm uppercase'>New York, NY</p>
-              <p>2022 - Present</p>
+              <p>2015 - 2020</p>
             </div>
           </div>
         </NavLink>
-        <VisiQLExp openVisiQL={openVisiQL} setOpenVisiQL={setOpenVisiQL} />
+        {/* @ts-ignore */}
+        <GreenhouseExp
+          openGreenhouse={openGreenhouse}
+          setOpenGreenhouse={setOpenGreenhouse}
+        />
       </div>
     </section>
   );
